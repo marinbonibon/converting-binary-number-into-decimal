aaaef54f1e898e64984f0142499fdcedb39aa74e
Maryna Petrenko <marinbonibon@gmail.com>
2019-04-02 15:54:22 +0400

    feat: create a function, that converts binary number to decimal

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const str = prompt("Enter binary number, please", "");
+
+function convert (str) {
+  const powOfTwo = str.length - 1;
+  const splitArr = str.split("");
+  let result = 0;
+
+  for (let i = 0; i < str.length; i++)  {
+
+    if(splitArr[i] === "1" || splitArr[i] === "0") {
+      result += splitArr[i] * Math.pow(2, powOfTwo - i);
+
+    } else {
+      return "It's not a binary number";
+    }
+  }
+
+  return result.toString();
+}
+
+(function () {
+  const p = document.createElement("p");
+  p.innerText = "Result is: " + convert(str);
+  document.body.appendChild(p);
+})();
